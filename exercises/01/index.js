405de2c4d0361d565ec5183b8f307fcd7faa4436
DanielaParra <dparralp396@gmail.com>
2023-02-09 19:04:33 -0500

    All done! Revamped HTML, completed JS functionality, added styles:fire:

--- exercises/01/index.js
+++ exercises/01/index.js
@@ -1,25 +1,37 @@
-const buttonElement = document.querySelector('button');
 const itemDescInput = document.querySelector('#description');
 const amountinput = document.querySelector('#amount');
-const ticketUL = document.querySelector('#ticket');
-
-const onButtonClick = () => {
-    buttonElement.addEventListener('click', (e) => {
-        const newListItem = document.createElement('li');
-        const newItemDescP = document.createElement('p');
-        const newAmountP = document.createElement('p');
-        newItemDescP.textContent = itemDescInput.value;
-        newAmountP.textContent = amountinput.value;
-        newListItem.append(newItemDescP, newAmountP);
-        ticketUL.append(newListItem);
-
-    })
-}
 
 const clearFields = () => {
     itemDescInput.value = '';
     amountinput.value = '';
 }
 
+const calculateTotal = (array) => {
+    const totalParagraph = document.querySelector('#total');
+    let sumTotal = 0;
+    array.forEach(number => sumTotal += number)
+    totalParagraph.textContent = `Total: $${sumTotal}`
+}
+
+const handleFormSubmit = () => {
+    const formELement = document.querySelector('form');
+    const ticketUL = document.querySelector('#ticket');
+    const totalArray = []
+    formELement.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const newListItem = document.createElement('li');
+        const newItemDescP = document.createElement('p');
+        const newAmountP = document.createElement('p');
+        newItemDescP.classList.add('capitalize')
+        newItemDescP.textContent = itemDescInput.value;
+        newAmountP.textContent = `$${amountinput.value}`;
+        newListItem.append(newItemDescP, newAmountP);
+        ticketUL.append(newListItem);
+        totalArray.push((Number(amountinput.value)));
+        calculateTotal(totalArray)
+        clearFields();
+    })
+}
+
 clearFields();
-onButtonClick();
+handleFormSubmit();
