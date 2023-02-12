ef0ac73cfc5cbcca6d51441cbdbec93f038c6e89
Poulami Saha <saha.poulami91@gmail.com>
2023-02-12 16:24:58 +0100

    css class update

--- exercises/02/index.js
+++ exercises/02/index.js
@@ -13,6 +13,7 @@ function playSound(e) {
   audio.currentTime = 0;
   audio.play();
 }
+
 const keys = document.querySelectorAll(".key");
 keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
