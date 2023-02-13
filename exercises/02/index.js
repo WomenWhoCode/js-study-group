a798ca193df4bccc0350d8887265e8d5e6dfd4b1
DanielaParra <dparralp396@gmail.com>
2023-02-12 22:07:51 -0500

    Added mouse click event listener, and css styling

--- exercises/02/index.js
+++ exercises/02/index.js
@@ -1,18 +1,39 @@
-const audioFiles = document.querySelectorAll('audio')
-console.log(audioFiles);
+const audioFiles = document.querySelectorAll('audio');
+
+const playAudio = (audioElement) => {
+    const audioFile = new Audio(audioElement.src);
+    setTimeout(() => {
+        audioFile.play()
+    }, 0);
+}
 
 const handleKeyPress = () => {
     document.addEventListener('keydown', (e) => {
-        const pressedKey = e.code;
+        const dataCode = e.code;
+        const dataKeyCode = e.keyCode;
         audioFiles.forEach((audio) => {
-            if (pressedKey === audio.dataset.key) {
-                const audioFile = new Audio(audio.src);
-                setTimeout(() => {
-                    audioFile.play()
-                }, 0);
+            if (dataCode !== undefined && dataCode === audio.dataset.code) {
+                playAudio(audio)
+            //Fallback in case browser does not support KeyboardEvent.code property and is still using deprecated KeyboardEvent.keyCode property instead
+            } else if (dataKeyCode === audio.dataset.keycode) {
+                playAudio(audio)
             }
         });
     });
 }
 
+const handleMouseClick = () => {
+    const keyDivs = document.querySelectorAll('.key');
+    keyDivs.forEach((div) => {
+        div.addEventListener('click', () => {
+            audioFiles.forEach((audio) => {
+                if (div.dataset.code === audio.dataset.code) {
+                    playAudio(audio);
+                }
+            });
+        });
+    });
+}
+
 handleKeyPress();
+handleMouseClick();
