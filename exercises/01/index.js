5a71b4b6f6cdc5c4ed87324d0f01682a8e0f4def
Emily Lubkert <elubkert@gmail.com>
2023-02-21 11:16:38 -0700

    update styling + move variable declarations

--- exercises/01/index.js
+++ exercises/01/index.js
@@ -1,11 +1,12 @@
 let receiptTable = document.getElementById('receipt-table');
-let article = document.getElementById('article').value;
-let value = parseInt(document.getElementById('amount').value);
 let element = document.getElementById('submit-btn');
 let total = 0;
 
 function onButtonClick(event) {
   event.preventDefault();
+
+  let article = document.getElementById('article').value;
+  let value = parseInt(document.getElementById('amount').value);
 
   let row = receiptTable.insertRow(-1);
   let cell1 = row.insertCell(0);
@@ -13,14 +14,13 @@ function onButtonClick(event) {
   cell1.classList.add('article-cell');
   cell2.classList.add('amount-cell');
 
-  cell1.innerHTML = article;
-  cell2.innerHTML = `$${value}`;
+  cell1.textContent = article;
+  cell2.textContent = `$${value}`;
 
   total += value;
-  document.getElementById('total-amount').innerHTML = `$${total}`;
+  document.getElementById('total-amount').textContent = `$${total}`;
 
   document.getElementById('form').reset();
 }
-
 
 element.addEventListener('click', onButtonClick);
