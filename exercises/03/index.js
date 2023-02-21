4c0b1b8aad5c1a4217f4a742ba23a985eb932fff
Emily Lubkert <elubkert@gmail.com>
2023-02-21 08:54:35 -0700

    initial commit, no styling

---
+++ exercises/03/index.js
@@ -0,0 +1,71 @@
+const calculator = document.querySelector('.calculator');
+const keys = document.querySelector('.calculator__keys');
+const display = document.querySelector('.calculator__display');
+
+keys.addEventListener('click', (event) => {
+  if (event.target.matches('button')) {
+    const key = event.target;
+    const action = key.dataset.action;
+    const keyContent = key.textContent;
+    const displayedNum = display.textContent;
+    const previousKeyType = calculator.dataset.previousKeyType;
+
+    if (!action) {
+      if (displayedNum === '0' || previousKeyType === 'operator') {
+        display.textContent = keyContent;
+      } else {
+        display.textContent = displayedNum + keyContent;
+      }
+    }
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.';
+    }
+
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      key.classList.add('is-pressed');
+      calculator.dataset.previousKeyType = 'operator';
+      calculator.dataset.firstValue = displayedNum;
+      calculator.dataset.operator = action;
+    }
+
+    Array.from(key.parentNode.children).forEach((key) =>
+      key.classList.remove('is-pressed')
+    );
+
+    if (action === 'clear') {
+      display.textContent = '';
+    }
+
+    if (action === 'equals') {
+      const firstValue = calculator.dataset.firstValue;
+      const operator = calculator.dataset.operator;
+      const secondValue = displayedNum;
+
+      display.textContent = calculate(firstValue, operator, secondValue);
+    }
+  }
+});
+
+function calculate(x, operator, y) {
+  let result = '';
+  switch (operator) {
+    case 'add':
+      result = parseFloat(x) + parseFloat(y);
+      break;
+    case 'subtract':
+      result = parseFloat(x) - parseFloat(y);
+      break;
+    case 'multiply':
+      result = parseFloat(x) * parseFloat(y);
+      break;
+    case 'divide':
+      result = parseFloat(x) / parseFloat(y);
+      break;
+  }
+  return result;
+}
