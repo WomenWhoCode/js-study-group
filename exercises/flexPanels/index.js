0c9a02da3ebc383f9d0456c8798420bfb1f4bee0
Emily Lubkert <elubkert@gmail.com>
2023-02-22 19:39:12 -0700

    add flex-panels

---
+++ exercises/flexPanels/index.js
@@ -0,0 +1,17 @@
+const panels = document.querySelectorAll('.panel');
+
+function toggleOpen(e) {
+  let panel = e.currentTarget;
+  panel.classList.toggle('open');
+}
+
+function toggleActive(e) {
+  if (e.propertyName.includes('flex')) {
+    this.classList.toggle('open-active');
+  }
+}
+
+panels.forEach((panel) => panel.addEventListener('click', toggleOpen));
+panels.forEach((panel) =>
+  panel.addEventListener('transitionend', toggleActive)
+);
