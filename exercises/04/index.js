a26d7ca01dfc4502a92845c825333474abc56766
daiterranova <daiana.terranova@gmail.com>
2023-02-26 18:17:38 -0300

    add event transitioned for moving text

--- exercises/04/index.js
+++ exercises/04/index.js
@@ -1,16 +1,16 @@
-//select panels
 const panels = document.querySelectorAll(".panel");
 
-//add an event listener to open panel and remove the open class
-// if were added in other panel
 panels.forEach((currentPanel) => {
   currentPanel.addEventListener("click", () => {
-    removeOpenClasses(currentPanel);
+    toggleOpenClasses(currentPanel);
     currentPanel.classList.toggle("open");
+    currentPanel.addEventListener("transitionend", () => {
+      currentPanel.classList.add("open-active");
+    });
   });
 });
 
-function removeOpenClasses(currentPanel) {
+function toggleOpenClasses(currentPanel) {
   panels.forEach((panel) => {
     if (panel != currentPanel) panel.classList.remove("open");
   });
